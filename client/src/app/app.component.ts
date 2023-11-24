ccf130d21bf1a2f581699e70acaa794990e71970
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-24 16:12:37 +0530

    Created Angular Application

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'client';
+  // @HostBinding('class.dark') get mode() => {
+  //   return
+  // }
+}
