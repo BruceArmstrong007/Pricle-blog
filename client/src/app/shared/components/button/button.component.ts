d3be27b50cf5739e4260eff30f67a93c2946d480
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-24 21:58:49 +0530

    added button component

---
+++ client/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <button class="hover:animate-pulse flex justify-evenly items-center gap-1 p-2">
+      <ng-content select="btn-prefix"></ng-content>
+      <ng-content select="btn-name"></ng-content>
+      <ng-content select="btn-suffix"></ng-content>
+    </button>
+  `,
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {}
