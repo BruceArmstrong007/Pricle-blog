71a319f4e3e8ce7a45ca1ee8b57bb79c9ee4fabd
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-24 16:56:24 +0530

    added ngrx packages

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })]
+    }), provideStore(), provideEffects(), provideRouterStore()]
 };
