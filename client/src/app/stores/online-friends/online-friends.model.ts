9adb2b063a2cb8dea87d578131b4de978e6ea8d4
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-11-24 18:09:25 +0530

    cleaning up ngrx files before setup

--- client/src/app/stores/online-friends/online-friends.model.ts
+++
@@ -1,9 +0,0 @@
-import { EntityState } from '@ngrx/entity';
-
-export interface OnlineUsers {
-  id: string;
-  socketID: string;
-  isOnline: boolean;
-}
-
-export interface OnlineUsersState extends EntityState<OnlineUsers> {}
