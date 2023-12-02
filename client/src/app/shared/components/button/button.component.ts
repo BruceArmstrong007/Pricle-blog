5eaf8e665a57dbdbf4cc2108b07006155d2ae1f1
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-02 18:56:05 +0530

    updated UI for input button, validation-error components,

--- client/src/app/shared/components/button/button.component.ts
+++ client/src/app/shared/components/button/button.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+type Buttontype = 'submit' | 'button';
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   template: `
-    <button class="hover:animate-pulse flex justify-evenly items-center gap-1 p-2">
+    <button [type]="btnType" [disabled]="disabled" class="hover:animate-pulse flex justify-evenly items-center gap-1 p-2">
       <ng-content select="btn-prefix"></ng-content>
       <ng-content select="btn-name"></ng-content>
       <ng-content select="btn-suffix"></ng-content>
@@ -14,6 +15,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-class ButtonComponent {}
+class ButtonComponent {
+  @Input() btnType!: Buttontype;
+  @Input() disabled: boolean | undefined;
+
+}
 
 export default ButtonComponent;
