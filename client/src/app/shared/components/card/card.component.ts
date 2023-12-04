f590e28a57e37c74981f3285363e24ef683fdf3a
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-04 20:45:42 +0530

    card ui changes

--- client/src/app/shared/components/card/card.component.ts
+++ client/src/app/shared/components/card/card.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="bg-slate-200 dark:bg-slate-700 rounded-xl p-4 flex flex-col justify-evenly items-center gap-1">
+    <div class="bg-slate-100 dark:bg-slate-700 rounded-xl p-4 flex flex-col justify-evenly items-center gap-1">
       <ng-content select="header"></ng-content>
       <ng-content select="body"></ng-content>
       <ng-content select="footer"></ng-content>
