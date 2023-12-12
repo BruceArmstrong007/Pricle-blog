a1f0df3998a13fd6c58df022356123e9a96d03d5
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-12 15:23:13 +0530

    Adding contact and dashboard routes

---
+++ client/src/app/routes/user/routes/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+class ContactComponent {
+
+}
+export default ContactComponent;
