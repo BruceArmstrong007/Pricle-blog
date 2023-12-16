45531fd7ad7ec6646f0fc0833778b1e14febf49b
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-16 21:13:31 +0530

    contact component changes for all the contact routes

--- client/src/app/routes/user/routes/contact/contact.component.ts
+++ client/src/app/routes/user/routes/contact/contact.component.ts
@@ -1,13 +1,64 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Signal,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import ButtonComponent from '../../../../shared/components/button/button.component';
+import CardComponent from '../../../../shared/components/card/card.component';
+import { Store } from '@ngrx/store';
+import { selectUrl } from '../../../../shared/router-store/router-selector';
+import {
+  RoutesInterface,
+  ClientRoutes,
+} from '../../../../shared/utils/client.routes';
+import ContactSkeletonComponent from './components/contact-skeleton/contact-skeleton.component';
+import { contactsFeature } from '../../../../stores/contacts/contacts.reducer';
+import DividerComponent from '../../../../shared/components/divider/divider.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    NgClass,
+    NgIf,
+    NgFor,
+    RouterLink,
+    RouterOutlet,
+    ButtonComponent,
+    CardComponent,
+    DividerComponent,
+    ContactSkeletonComponent
+  ],
   templateUrl: './contact.component.html',
-  styles: ``,
+  styles: `
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-class ContactComponent {}
+class ContactComponent {
+  private readonly store = inject(Store);
+  readonly routePath = this.store.selectSignal(selectUrl);
+  readonly Routes: Signal<RoutesInterface> = signal(ClientRoutes);
+  readonly contactState = this.store.selectSignal(contactsFeature.selectState);
+  readonly currentRoute = computed(() => {
+    let currentRoute;
+    const route = this.routePath().split('/').pop();
+    switch (route) {
+      case 'friend-requests':
+        currentRoute = 'Friend Requests';
+        break;
+      case 'received-requests':
+        currentRoute = 'Received Requests';
+        break;
+      case 'friends':
+      default:
+        currentRoute = 'Friends';
+    }
+    return currentRoute;
+  });
+}
 export default ContactComponent;
