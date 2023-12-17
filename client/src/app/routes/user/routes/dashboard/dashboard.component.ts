96e84ab3c99a2f1f5eb67b44763e6efcc109f64a
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-17 16:55:21 +0530

    Dashboard setup for search

--- client/src/app/routes/user/routes/dashboard/dashboard.component.ts
+++ client/src/app/routes/user/routes/dashboard/dashboard.component.ts
@@ -1,21 +1,54 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
 import ButtonComponent from '../../../../shared/components/button/button.component';
 import InputComponent from '../../../../shared/components/input/input.component';
+import { ClientRoutes } from '../../../../shared/utils/client.routes';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [RouterOutlet, ReactiveFormsModule, InputComponent, ButtonComponent],
   templateUrl: './dashboard.component.html',
-  styles: ``,
+  styles: `
+  .height {
+    height: calc(100vh - 130px);
+  }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 class DashboardComponent {
   private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly Routes = signal(ClientRoutes);
   readonly form = this.fb.group({
-    search: [''],
+    search: ['', Validators.pattern('^[a-zA-Z]|^@[a-zA-Z]|^#[a-zA-Z].*')],
   });
+
+  submit() {
+    if (this.form.invalid) return;
+    const key = this.form.value.search?.charAt(0);
+    let params = '';
+    switch (key) {
+      case '#':
+        params = JSON.stringify({ search: this.form.value, type: 'tag' });
+        break;
+      case '@':
+        params = JSON.stringify({ search: this.form.value, type: 'username' });
+        break;
+      default:
+        params = JSON.stringify({ search: this.form.value, type: 'name' });
+    }
+
+    this.router.navigate([
+      this.Routes().User.Dashboard.Search,
+      { queryParams: params },
+    ]);
+  }
 }
 export default DashboardComponent;
