95515d7f7d9c395598be4721af4f72ff591dd3a7
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-24 22:39:09 +0530

    screen height fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,7 +9,6 @@ import { ModeService } from './shared/services/mode/mode.service';
 import { AsyncPipe, NgComponentOutlet, NgFor } from '@angular/common';
 import AlertPopupComponent from './shared/components/alert-popup/alert-popup.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -21,12 +20,16 @@ import AlertPopupComponent from './shared/components/alert-popup/alert-popup.com
     AlertPopupComponent,
   ],
   template: `
-    <div class="dark:bg-black dark:text-white w-full h-screen">
+    <div class="dark:bg-black dark:text-white w-full screen">
       <router-outlet />
     </div>
     <app-alert-popup />
   `,
-  styles: ``,
+  styles: `
+  .screen {
+    height: 100dvh;
+  }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 class AppComponent {
