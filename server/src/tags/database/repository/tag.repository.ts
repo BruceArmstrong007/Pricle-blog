3a70558da8c4110efc4be82b34da7c70fe4ada19
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-24 10:43:41 +0530

    clean up tag repo

--- server/src/tags/database/repository/tag.repository.ts
+++ server/src/tags/database/repository/tag.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
-import { ContactStatus } from '@app/common';
+import { Model } from 'mongoose';
 import { Tag } from '../schema/tag.schema';
 @Injectable()
 export class TagRepository {
