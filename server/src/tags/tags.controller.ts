0b9e10e8213bac03639ea22af0a0ff02d446fff2
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-24 10:14:00 +0530

    protected tag routes with jwtAuthGuard and added global exceptionfilterhandling,  + clean up

--- server/src/tags/tags.controller.ts
+++ server/src/tags/tags.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { TagsService } from './tags.service';
-import { CreateTagDto, GetTags } from './dto/tag.request';
-import { UpdateTagDto } from './dto/update-tag.dto';
+import { CreateTag, GetTags } from './dto/tag.request';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ApiExceptionFilter } from '@app/common';
 
 @Controller('tags')
+@UseGuards(JwtAuthGuard)
+@UseFilters(new ApiExceptionFilter())
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
   @Post()
-  create(@Body() createTagDto: CreateTagDto) {
+  create(@Body() createTagDto: CreateTag) {
     return this.tagsService.create(createTagDto.name);
   }
 
