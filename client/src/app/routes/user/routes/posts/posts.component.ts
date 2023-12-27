a5c627d3e4a842c654c171c9b253f19ff091b93b
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-12-27 18:04:05 +0530

    cleanup post routes component

--- client/src/app/routes/user/routes/posts/posts.component.ts
+++ client/src/app/routes/user/routes/posts/posts.component.ts
@@ -1,13 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import CardComponent from '../../../../shared/components/card/card.component';
+import { Store } from '@ngrx/store';
+import { selectUrl } from '../../../../shared/router-store/router-selector';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './posts.component.html',
-  styles: ``,
+  styles: `
+    .height {
+    height: calc(100dvh - 200px);
+  }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-class PostsComponent {}
+class PostsComponent {
+  private readonly store = inject(Store);
+  readonly routePath = this.store.selectSignal(selectUrl);
+  readonly currentRoute = computed(() => {
+    let currentRoute;
+    let route = this.routePath().split('/').pop();
+    switch (route) {
+      case 'create':
+        currentRoute = 'Create Post';
+        break;
+      case 'edit':
+        currentRoute = 'Edit Post';
+        break;
+      default:
+        currentRoute = 'Create Post';
+        break;
+    }
+    return currentRoute;
+  });
+}
 export default PostsComponent;
