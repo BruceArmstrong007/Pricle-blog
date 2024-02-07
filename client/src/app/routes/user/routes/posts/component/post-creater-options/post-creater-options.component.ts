64a276cc35bfb544e9167cc6a5d0d7d30d26e8b3
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-07 19:04:06 +0530

    Added simple tooltip - Create posts route

--- client/src/app/routes/user/routes/posts/component/post-creater-options/post-creater-options.component.ts
+++ client/src/app/routes/user/routes/posts/component/post-creater-options/post-creater-options.component.ts
@@ -1,7 +1,22 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 import ButtonComponent from '../../../../../../shared/components/button/button.component';
 
-export type createPostOption = 'heading' | 'paragraph' | 'unordered_list' | 'ordered_list' | 'divider' | 'code_block' | 'table' | 'task_list' | 'image' | 'block_quote';
+export type createPostOption =
+  | 'heading'
+  | 'paragraph'
+  | 'unordered_list'
+  | 'ordered_list'
+  | 'divider'
+  | 'code_block'
+  | 'table'
+  | 'task_list'
+  | 'image'
+  | 'block_quote';
 
 @Component({
   selector: 'app-post-creater-options',
@@ -10,10 +25,21 @@ export type createPostOption = 'heading' | 'paragraph' | 'unordered_list' | 'ord
   templateUrl: './post-creater-options.component.html',
   styles: `
 
+  .tooltip:hover::before {
+      content: attr(data-tooltip);
+      position: absolute;
+      left: -50%; /* Adjust this value based on your desired tooltip position */
+      background-color: #333;
+      color: white;
+      padding: 5px;
+      border-radius: 5px;
+      white-space: nowrap;
+    }
+
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 class PostCreaterOptionsComponent {
- @Output() optionEvent = new EventEmitter<createPostOption>();
+  @Output() optionEvent = new EventEmitter<createPostOption>();
 }
 export default PostCreaterOptionsComponent;
